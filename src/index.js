fc23d19010e94f551cc6bbc61181b93ac7e2a958
Sindhya Kumari <sindhyakumari93@gmail.com>
2021-10-25 03:07:24 +1100

    taking user input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-const sayHello = (name = "Haz") => `Hello, ${name}!`;
+// const sayHello = (name = "Haz") => `Hello, ${name}!`;
 
-export default sayHello;
+// export default sayHello;
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+const integersString = prompt(
+  "Input any number of integers separated by single whitespace such as 6 1 5 3 2: "
+);
+console.log("integersString :>> ", integersString);
+
+// function name(params) {
+
+// }
+
+// const name = (params) => {}
+
+// map
+// filter
+// reduce
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+
+// array.forEach(element => {
+
+// });
